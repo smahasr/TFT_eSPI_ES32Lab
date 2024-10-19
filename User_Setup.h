6927fe6b602d7a11b5f4a76123ef41d282d53e8f
Mahasri <158250920+smahasr@users.noreply.github.com>
2024-10-19 15:15:46 +0530

    Update User_Setup.h
    Updated pin configurations for ESP32

--- User_Setup.h
+++ User_Setup.h
@@ -1,21 +1,26 @@
-#define ST7735_DRIVER      // Define additional parameters below for this display
+#define ILI9341_DRIVER // Use ILI9341 driver
 
-#define TFT_DC   2  // A0/DC   Data Command control pin
-#define TFT_CS   15  // CS      Chip select control pin
-#define TFT_MOSI 23  // SDA
-#define TFT_SCLK 18  // SCK/SCL
-#define TFT_BL   -1  // LED/BLK LED back-light (required for M5Stack)
-#define TFT_RST  -1  // RST     Reset pin (could connect to RST pin) - 
+#define TFT_RST 16 // Reset pin
+#define TFT_CS 5   // Chip select control pin
+#define TFT_DC 4   // Data Command control pin
+#define TFT_WR 17  // Write strobe control pin
+#define TFT_RD -1  // No read pin used
 
-#define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
-#define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
-#define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
-#define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
-#define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
-#define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
-#define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
+// Data pins
+#define TFT_D0 12
+#define TFT_D1 13
+#define TFT_D2 26
+#define TFT_D3 25
+#define TFT_D4 33
+#define TFT_D5 32
+#define TFT_D6 27
+#define TFT_D7 14
 
+#define LOAD_GLCD   // Font 1
+#define LOAD_FONT2  // Font 2
+#define LOAD_FONT4  // Font 4
+#define LOAD_FONT6  // Font 6
+#define LOAD_FONT7  // Font 7
+#define LOAD_FONT8  // Font 8
+#define LOAD_GFXFF  // FreeFonts
 #define SMOOTH_FONT
-#define SPI_FREQUENCY  27000000
-#define SPI_READ_FREQUENCY  20000000
-#define SPI_TOUCH_FREQUENCY  2500000
